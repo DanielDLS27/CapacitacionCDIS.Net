9e78b8ce1a4d9d51e5a10a09555cea751fff6ad6
DanielDLS27 <daniel.deleonsl@uanl.edu.mx>
2022-03-29 21:05:38 -0600

    Evidencia 1 lista

---
+++ Etapa 1/E1_Actividad1/E1_Actividad1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1_Actividad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Altura: ");
+
+            int altura = int.Parse(Console.ReadLine());
+
+            while(altura < 0 || altura > 25)
+            {
+                Console.Write("Altura: ");
+                altura = int.Parse(Console.ReadLine());
+            }
+
+            int aux = altura;
+
+            for(int i = 0; i < altura; i++)
+            {
+                Console.WriteLine();
+                for(int j = aux-1; j >= 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                aux--;
+                for(int k = 0; k <= i; k++)
+                {
+                    Console.Write("#");
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}

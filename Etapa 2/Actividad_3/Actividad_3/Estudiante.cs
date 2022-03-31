c265b802f79c7e04e0180fdf97107c8d80ab729a
DanielDLS27 <daniel.deleonsl@uanl.edu.mx>
2022-03-30 19:34:51 -0600

    Actividad 3 lista

---
+++ Etapa 2/Actividad_3/Actividad_3/Estudiante.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_3
+{
+    internal class Estudiante
+    {
+        private string matricula;
+        private string nombre;
+        private int edad;
+        private string password;
+
+        public Estudiante()
+        {
+
+        }
+
+        public Estudiante(string matricula, string nombre, int edad)
+        {
+            this.matricula = matricula;
+            this.nombre = nombre;
+            asignarEdad(edad);
+            this.password = asignarPassword();
+        }
+
+        public void asignarEdad(int edad)
+        {
+            if(edad >= 15 && edad <= 90)
+            {
+                this.edad = edad;
+            }
+            else
+            {
+                Console.WriteLine("La edad debe estar entre 15 y 90 años");
+            }
+        }
+
+        public string asignarPassword()
+        {
+            var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var Charsarr = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < Charsarr.Length; i++)
+            {
+                Charsarr[i] = characters[random.Next(characters.Length)];
+            }
+
+            var resultString = new String(Charsarr);
+            return resultString;
+        }
+
+        public string mostrarDatos()
+        {
+            return "Matricula: "+this.matricula+"\n"+
+                    "Nombre: "+this.nombre+"\n"+
+                    "Edad: "+edad+"\n"+
+                    "Password: " + this.password+"\n";
+        }
+    }
+}
---
+++ Etapa 2/Actividad_3/Actividad_3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante[] estudiantes;
+            int cantEstudiantes;
+            string matricula;
+            string nombre;
+            int edad;
+
+            Console.WriteLine("Ingrese cantidad de estudiantes: ");
+            cantEstudiantes = int.Parse(Console.ReadLine());
+
+            estudiantes = new Estudiante[cantEstudiantes];
+
+            for(int i = 0; i < estudiantes.Length; i++)
+            {
+                Console.WriteLine("Estudiante #" +( i + 1));
+                Console.Write("Ingrese Matricula: ");
+                matricula = Console.ReadLine();
+                Console.Write("\nIngrese Nombre: ");
+                nombre = Console.ReadLine();
+                Console.Write("\nIngrese Edad: ");
+                edad = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                estudiantes[i] = new Estudiante(matricula, nombre, edad);
+            }
+
+            for(int i = 0;i < estudiantes.Length; i++)
+            {
+                Console.WriteLine("Estudiante #"+( i + 1)+"\n");
+                Console.WriteLine(estudiantes[i].mostrarDatos());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
